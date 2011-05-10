deccd43618e850e8f0eee0e3d41fef18f087951d
Emerson Lackey <emerson@plankdesign.com>
2011-05-10 17:56:32 -0400

    adding first PHP file and plugin definition

---
+++ highly-classified.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Highly Classified
+Plugin URI: http://www.emersonlackey.com/highly-classified/
+Description: A simple and clean Classified ad plugin written for Wordpress.
+Version: 0.1
+Author: Emerson Lackey
+Author URI: http://www.emersonlackey.com
+License: GPL2
+*/
+?>
